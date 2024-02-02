d82654f622b37a9f8a643d672f9de7c5169f9aba
jxaovito <joaovitoorvieiraborba@gmail.com>
2024-02-02 08:11:23 -0300

    lifecycle hooks

--- src/App.vue
+++ src/App.vue
@@ -8,10 +8,78 @@ export default {
       pClass: ["title"],
       nome: 'jon snow',
       itsTrue: true,
+      user: {
+            first_name: 'Jon',
+            last_name: 'Snow'
+        },
       players: '',
       // colors recebe array para o checkbox funcionar
-      colors: []
+      colors: [],
+       todos: [{
+            
+  
+    "userId": 1,
+    "id": 1,
+    "title": "sunt aut facere repellat provident occaecati excepturi optio reprehenderit",
+    "body": "quia et suscipit\nsuscipit recusandae consequuntur expedita et cum\nreprehenderit molestiae ut ut quas totam\nnostrum rerum est autem sunt rem eveniet architecto"
+  },
+  {
+    "userId": 1,
+    "id": 2,
+    "title": "qui est esse",
+    "body": "est rerum tempore vitae\nsequi sint nihil reprehenderit dolor beatae ea dolores neque\nfugiat blanditiis voluptate porro vel nihil molestiae ut reiciendis\nqui aperiam non debitis possimus qui neque nisi nulla"
+  },
+  {
+    "userId": 1,
+    "id": 3,
+    "title": "ea molestias quasi exercitationem repellat qui ipsa sit aut",
+    "body": "et iusto sed quo iure\nvoluptatem occaecati omnis eligendi aut ad\nvoluptatem doloribus vel accusantium quis pariatur\nmolestiae porro eius odio et labore et velit aut"
+  },
+  {
+    "userId": 1,
+    "id": 4,
+    "title": "eum et est occaecati",
+    "body": "ullam et saepe reiciendis voluptatem adipisci\nsit amet autem assumenda provident rerum culpa\nquis hic commodi nesciunt rem tenetur doloremque ipsam iure\nquis sunt voluptatem rerum illo velit"
+  }],
+  name: '',
+  pageCount: 5,
     };
+  },
+  beforeCreate(){
+    console.log('beforeCreate');
+    console.log('Estado: ', this.nome);
+    console.log('DOM: ', this.$el);
+  },
+  created(){
+    console.log('created');
+    console.log('Estado: ', this.nome);
+    console.log('DOM: ', this.$el);
+},
+beforeMount(){
+  console.log('beforeMount');
+    console.log('Estado: ', this.nome);
+    console.log('DOM: ', this.$el);
+},
+mounted(){
+    window.addEventListener('resize', this.resize);
+},
+beforeUnmount(){
+  window.removeEventListener('resize', this.resize);
+},
+unmounted(){
+  console.log('unmounted');
+    console.log('Estado: ', this.nome);
+    console.log('DOM: ', this.$el);
+},
+  watch: {
+    name() {
+      this.saveUserName();
+    },
+    pageCount() {
+      this.changePage();
+     
+    }
+   
   },
   methods: {
     changeName() {
@@ -28,8 +96,28 @@ export default {
     },
     onKeyUp($evt){
       console.log('key up', $evt);
+    },
+    saveUserName(){
+      console.log(this.name);
+    },
+    changePage(){
+      console.log('page changed');
+    },
+    resize($evt){
+      console.log($evt);
     }
-  }
+  },
+  computed: {
+    fullName() {
+        return `${this.user.first_name} ${this.user.last_name}`
+    },
+    uncompletedTodos() {
+        return this.todos.filter(todo => !todo.completed);
+    },
+    completedTodos() {
+        return this.todos.filter(todo => todo.completed);
+    }
+  },
 };
 
 </script>
@@ -74,6 +162,37 @@ export default {
 <br>
  <!-- keyboard event .enter só dispara quando aperta enter -->
   <input type="text" @keyup.enter="onKeyUp">
+
+  <div>
+    {{ fullName }}
+    <br> <br>
+    <h2>Todos em aberto</h2>
+    <div v-for="todo in uncompletedTodos" :key="todo.id">
+        {{ todo.title }} <span>{{ todo.completed }}</span>
+    </div>
+    <br>
+    <h2>Todos completas</h2>
+    <div v-for="todo in completedTodos" :key="todo.id">
+        {{ todo.title }} <span>{{ todo.completed }}</span>
+    </div>
+    <br>
+    <h2>Todos</h2>
+    <div v-for="todo in todos" :key="todo.id">
+    <input type="checkbox"
+    v-model="todo.completed">
+        {{ todo.title }}
+</div>
+</div>
+
+<input type="text" v-model="name"> <br>
+<span>{{ name }}</span>
+<br><br>
+<select v-model="pageCount">
+<option value="5">5</option>
+<option value="10">10</option>
+<option value="15">15</option>
+</select> <br>
+{{ pageCount }}
 </div>
 
 
